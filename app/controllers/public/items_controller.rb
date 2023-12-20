9a122e80f5a7b07c13fa927e6faa5ccfb244100e
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-20 07:26:27 +0000

    会員・商品view

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,26 @@
 class Public::ItemsController < ApplicationController
-  
-  def edit
-  end
+  before_action :ensure_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.page(params[:page])
   end
 
   def new
+    @item = Item.new
   end
 
   def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
