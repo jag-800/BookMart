c754db027c8439f48b62278e8367e0bebc31d102
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-22 14:24:15 +0000

    admin/ステータス切り替え

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,20 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :ensure_item, only: [:show, :edit, :update]
+
+
+  def index
+    @items = Item.page(params[:page])
+    @all_items_count = @items.count
+  end
+
   def show
   end
 
-  def update
+  private
+
+
+  def ensure_item
+    @item = Item.find(params[:id])
   end
 end
