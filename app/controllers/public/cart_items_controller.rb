945521d29aea09a6a409a06fccf2879cce9d9dd3
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-22 06:53:02 +0000

    カート機能完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,50 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_cart_item, only: [:create, :update, :destroy]
+
   def index
+    @cart_items = current_customer.cart_items.includes(:item)
+  end
+
+  def create
+    if @cart_item
+      new_amount = @cart_item.amount + cart_item_params[:amount].to_i
+      @cart_item.update(amount: new_amount)
+      redirect_to cart_items_path
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      @cart_item.item_id = @item.id
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        render 'public/items/show'
+      end
+    end
+  end
+
+  def update
+    @cart_item.update(cart_item_params) if @cart_item
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    @cart_item.destroy if @cart_item
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
+  end
+
+  def set_cart_item
+    @item = Item.find(params[:item_id])
+    @cart_item = current_customer.has_in_cart(@item)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -23,4 +23,8 @@ class Customer < ApplicationRecord
   def full_name_kana
     last_name_kana + " " + first_name_kana
   end
+  
+  def has_in_cart(item)
+    cart_items.find_by(item_id: item.id)
+  end
 end
