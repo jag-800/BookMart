a1f3419e09c62121de5007030ae10424ffdc247b
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-22 09:09:02 +0000

    完了配送先機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -11,22 +11,37 @@ class Public::AddressesController < ApplicationController
   end
   
   def create
+    @addresses = current_customer.addresses
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
   
   def update
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address.destroy
+    redirect_to addresses_path
   end
   
   private
   
   def address_params
-    params.arequire(:address).permit(:post_code, :address, :name)
+    params.require(:address).permit(:post_code, :address, :name)
   end
   
   def ensure_address
-    @addresses = current_cusotmer.addresses
+    @addresses = current_customer.addresses
     @address = @addresses.find_by(id: params[:id])
     redirect_to addresses_path unless @address
   end
