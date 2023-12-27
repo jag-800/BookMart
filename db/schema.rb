2a86bb0b79ea20aab961e20105105ce99482787e
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-27 08:01:05 +0000

    remove/addresses

--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,6 @@ ActiveRecord::Schema.define(version: 2023_12_22_161043) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.string "name", null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
