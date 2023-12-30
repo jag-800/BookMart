dbe761e9bdbc7c2f2ae8bd64c7850f365c5e2e6e
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-30 09:27:28 +0000

    レイアウト/show

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,20 +1,22 @@
 class Public::ChatsController < ApplicationController
 
   def show
-    @customer = Customer.find(params[:id]) #チャットする相手は誰？
-    rooms = current_customer.customer_rooms.pluck(:room_id) #ログイン中のユーザーの部屋情報を全て取得
-    customer_rooms = CustomerRoom.find_by(customer_id: @customer.id, room_id: rooms)#その中にチャットする相手とのルームがあるか確認
+    @customer = Customer.find(params[:id]) #相手のid 14行目で使う
+    rooms = current_customer.customer_rooms.pluck(:room_id)
+    customer_rooms = CustomerRoom.find_by(customer_id: @customer.id, room_id: rooms)
 
-    unless customer_rooms.nil?#ユーザールームがある場合
-      @room = customer_rooms.room#変数@roomにユーザー（自分と相手）と紐づいているroomを代入
-    else#ユーザールームが無かった場合
-      @room = Room.new#新しくRoomを作る
-      @room.save#そして保存
-      CustomerRoom.create(customer_id: current_customer.id, room_id: @room.id)#自分の中間テーブルを作成
-      CustomerRoom.create(customer_id: @customer.id, room_id: @room.id)#相手の中間テーブルを作成
+    unless customer_rooms.nil? #roomがある時
+      @room = customer_rooms.room
+    else
+      @room = Room.new
+      @room.save
+      CustomerRoom.create(customer_id: current_customer.id, room_id: @room.id)
+      CustomerRoom.create(customer_id: @customer.id, room_id: @room.id)
     end
-    @chats = @room.chats#チャットの一覧
-    @chat = Chat.new(room_id: @room.id)#チャットの投稿
+
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
+    @my_name = "自分"
   end
 
   def create
@@ -29,4 +31,3 @@ class Public::ChatsController < ApplicationController
     params.require(:chat).permit(:message, :room_id)
   end
 end
-  
