778fc738397f1b184188c92698bc2bd5ecb48e20
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2024-01-04 08:23:43 +0000

    admin/customerステータス更新

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,5 +14,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:alert] = "会員ステータスを変更しました。"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:is_active)
+  end
+  
 end
