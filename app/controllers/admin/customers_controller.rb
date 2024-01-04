9f35a7bfab4c25b1abaa0aa4505ee8aa080e1d83
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2024-01-04 07:49:17 +0000

    admin/customer_show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,6 +5,12 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @items = @customer.items
+    @tags = @items.flat_map(&:tag_list).uniq
+    @tag_counts = @items.each_with_object(Hash.new(0)) do |item, counts|
+      item.tag_list.each { |tag| counts[tag] += 1 }
+    end
   end
 
   def edit
