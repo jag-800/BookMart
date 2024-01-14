5372c5ad373f2a35fd18d7ef1f3c49cd36a2df3d
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2024-01-14 21:10:15 +0000

    fix/headerレイアウト

--- app/javascript/packs/slick.js
+++ app/javascript/packs/slick.js
@@ -1,25 +1,25 @@
 /*global $*/
-$(window).on('load resize', function(){
-  var wid = window.innerWidth;
-  if (wid >= 800) {
-    $(document).ready(function(){
-      $('.slider').slick({
-        centerMode: true,
-        slidesToShow: 3,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        speed: 700,
-      });
-    });
-  } 
-  else {
-    $(document).ready(function(){
-      $('.slider').slick({
-        autoplay: true,
-        autoplaySpeed: 1500,
-        speed: 700,
-      });
-    });
-  }
+$(document).ready(function() {
+  var slickOptions = {
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 700
+  };
+
+  $(window).on('load resize', function() {
+    var wid = window.innerWidth;
+    if (wid >= 800) {
+      // 800px以上の場合のオプションを追加
+      $('.slider').slick('slickSetOption', 'centerMode', true, true);
+      $('.slider').slick('slickSetOption', 'slidesToShow', 3, true);
+      $('.slider').slick('slickSetOption', 'arrows', false, true);
+    } else {
+      // 800px未満の場合のオプションを追加
+      $('.slider').slick('slickSetOption', 'centerMode', false, true);
+      $('.slider').slick('slickSetOption', 'slidesToShow', 1, true);
+      $('.slider').slick('slickSetOption', 'arrows', true, true);
+    }
+  });
+
+  $('.slider').slick(slickOptions);
 });
